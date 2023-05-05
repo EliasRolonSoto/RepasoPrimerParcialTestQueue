61e284cdcd3fcc5af319e4a4846ca233b1d8eb53
Elias <eliasrolonsoto@gmail.com>
2023-05-05 11:44:57 -0300

    Add project files.

---
+++ RepasoPrimerParcial/Alumno.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RepasoPrimerParcial
+{
+    public class Alumno
+    {
+
+        public string _nombre { get; set; }
+        public string _apellido { get; set;}
+        public string _legajo { get; set; }
+
+        public Alumno() { }
+    }
+}
---
+++ TestRepaso1erParcial/UnitTest1.cs
@@ -0,0 +1,35 @@
+using RepasoPrimerParcial;
+using System.Security.Cryptography;
+
+namespace TestRepaso1erParcial
+{
+    public class UnitTest1
+    {
+        private Queue<Alumno> Alumnos = new Queue<Alumno>();
+        [Fact]
+
+        public void TieneQueCrear100milAlumnosYBuscarQuienTerminaEn11()
+        {
+
+            for (int i = 0; i < 100000; i++)
+            {
+                Alumno alumno = new Alumno();
+                alumno._nombre = ($"nombre {i}");
+                alumno._apellido = ($"Apellid {i}");
+                alumno._legajo = ($"Legajo: {i}");
+                Alumnos.Enqueue(alumno);
+            }
+
+
+            Assert.Equal(100000, Alumnos.Count);
+
+            //var cantidadordenada = Alumnos.OrderByDescending(alumno => alumno._nombre); ordenar lista
+             var cantidad = (from alumno in Alumnos
+                             where alumno._apellido.EndsWith("11")
+                             select alumno._apellido
+                            );
+
+            Assert.Equal(1000, cantidad.Count());
+        }
+    }
+}
