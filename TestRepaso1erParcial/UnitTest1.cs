9132800db74ad257dddc8512d8a1c74a30d239a6
Elias <eliasrolonsoto@gmail.com>
2023-05-08 21:07:00 -0300

    Formas de encotnrar el max

--- TestRepaso1erParcial/UnitTest1.cs
+++ TestRepaso1erParcial/UnitTest1.cs
@@ -24,12 +24,19 @@ namespace TestRepaso1erParcial
             Assert.Equal(100000, Alumnos.Count);
 
             //var cantidadordenada = Alumnos.OrderByDescending(alumno => alumno._nombre); ordenar lista
-             var cantidad = (from alumno in Alumnos
+
+            var cantidadMayor = (from alumno in Alumnos
+                                 select alumno._legajo).Max();
+                                           
+
+
+            var cantidad = (from alumno in Alumnos
                              where alumno._apellido.EndsWith("11")
                              select alumno._apellido
                             );
 
             Assert.Equal(1000, cantidad.Count());
+            Assert.Equal("Legajo: 99999", cantidadMayor);
         }
     }
 }
